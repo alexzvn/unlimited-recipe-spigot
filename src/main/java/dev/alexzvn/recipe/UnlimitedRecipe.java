c0ce2a774d6b7e1547d5fbbf6a8272c23a7e6d88
Alex <41188285+alexzvn@users.noreply.github.com>
2021-05-03 12:33:46 +0000

    update entry plugin

--- src/main/java/dev/alexzvn/recipe/UnlimitedRecipe.java
+++ src/main/java/dev/alexzvn/recipe/UnlimitedRecipe.java
@@ -1,27 +1,61 @@
 package dev.alexzvn.recipe;
 
-import org.bukkit.Bukkit;
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dev.alexzvn.recipe.helper.Util;
 import dev.alexzvn.recipe.listeners.PlayerSwitchItem;
+import dev.alexzvn.recipe.recipe.RecipeManager;
 
 /**
  * UnlimitedRecipe
  */
 public class UnlimitedRecipe extends JavaPlugin {
 
+    protected static UnlimitedRecipe instance;
+
+    protected RecipeManager recipeManager;
+
     @Override
     public void onEnable() {
+        instance = this;
+
         this.registerListeners();
-        this.getLogger().info("Plugin Recipe Enabled");
+        this.saveDefaultConfig();
+        this.createDefaultFolder();
+        this.registerRecipeManager();
     }
 
     @Override
     public void onDisable() {
-        
+        instance = null;
+        recipeManager = null;
+    }
+
+    protected void createDefaultFolder() {
+        String[] folders = {"recipes"};
+
+        for (String folder : folders) {
+            Util.mkdir(folder);
+        }
+    }
+
+    protected void registerRecipeManager() {
+        Logger logger = this.getLogger();
+
+        logger.info("Loading all recipes");
+
+        this.recipeManager = new RecipeManager();
+
+        logger.info("Success loaded " + recipeManager.count() + " recipes");
     }
 
     protected void registerListeners() {
-        Bukkit.getPluginManager().registerEvents(new PlayerSwitchItem(this), this);
+        Util.registerListener(new PlayerSwitchItem(this));
+    }
+
+    public static UnlimitedRecipe getInstance() {
+        return instance;
     }
 }
