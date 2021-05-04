d7e4d9d759df31f73a06902d0873826338fc477a
Alex <41188285+alexzvn@users.noreply.github.com>
2021-05-04 10:39:16 +0000

    add inventory listener hanler

---
+++ src/main/java/dev/alexzvn/recipe/listeners/InventoryListener.java
@@ -0,0 +1,22 @@
+package dev.alexzvn.recipe.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+import dev.alexzvn.recipe.ui.WorkbenchTable;
+
+public class InventoryListener implements Listener {
+
+    @EventHandler
+    public void clickEventHandler(InventoryClickEvent event) {
+        WorkbenchTable.handleClickEvent(event);
+    }
+
+    @EventHandler
+    public void closeEventHandler(InventoryCloseEvent event) {
+        WorkbenchTable.handleCloseEvent(event);
+    }
+
+}
