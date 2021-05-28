7181ff17da426166dafbd418035545fecf7ee277
Alex <41188285+alexzvn@users.noreply.github.com>
2021-05-28 00:18:10 +0000

    add category

---
+++ src/main/java/dev/alexzvn/recipe/helper/Category.java
@@ -0,0 +1,56 @@
+package dev.alexzvn.recipe.helper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+
+public class Category {
+
+    final protected static FileConfiguration config = Config.config();
+
+    public static Map<String, ItemStack> items() {
+        Map<String, ItemStack> items = new HashMap<String, ItemStack>();
+
+        for (String category : keys()) {
+            items.put(category, createItemCategory(category));
+        }
+
+        return items;
+    }
+
+    public static List<String> keys() {
+        return config.getStringList("categories");
+    }
+
+    public static boolean hasAny() {
+        return keys().size() > 0;
+    }
+
+    public static boolean isCategory(ItemStack item) {
+        return getCategory(item) != null;
+    }
+
+    public static String getCategory(ItemStack item) {
+        return Item.getString(item, "category");
+    }
+
+    protected static ItemStack createItemCategory(String key) {
+        String category = "categories." + key + ".",
+            name = config.getString(category + "name"),
+            material = config.getString(category + "material");
+
+        List<String> lore = config.getStringList(category + "lore");
+
+        ItemStack item = new ItemStack(Material.getMaterial(material));
+
+        Item.setName(item, name);
+        Item.setLore(item, lore);
+        Item.setString(item, "category", key);
+
+        return item;
+    }
+}
